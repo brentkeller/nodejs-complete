825b242864505ce9c84cdc3b3ecb02623eabe061
Brent Keller <brentkeller@gmail.com>
2020-01-09 14:05:41 -0500

    Add downloading pdf invoices from disk
    This leveraged a dummy PDF file named for the order ID

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,3 +1,6 @@
+const fs = require('fs');
+const path = require('path');
+
 const Product = require('../models/product');
 const Order = require('../models/order');
 
@@ -143,4 +146,35 @@ exports.postCreateOrder = (req, res, next) => {
       error.httpStatusCode = 500;
       return next(error);
     });
+};
+
+exports.getInvoice = (req, res, next) => {
+  const orderId = req.params.orderId;
+
+  Order.findById(orderId)
+    .then(order => {
+      if (!order) return next(new Error('Order not found'));
+      if (order.user.userId.toString() !== req.user._id.toString())
+        return next(new Error('Order not found'));
+      const invoiceName = `invoice-${orderId}.pdf`;
+      const invoicePath = path.join('data', 'invoices', invoiceName);
+
+      // // preloading data in memory before serving
+      // fs.readFile(invoicePath, (err, data) => {
+      //   if (err) return next(err);
+      //   res.setHeader('Content-Type', 'application/pdf');
+      //   res.setHeader(
+      //     'Content-Disposition',
+      //     `inline; filename="${orderId}.pdf"`,
+      //   );
+      //   res.send(data);
+      // });
+
+      // stream file to response
+      const file = fs.createReadStream(invoicePath);
+      res.setHeader('Content-Type', 'application/pdf');
+      res.setHeader('Content-Disposition', `inline; filename="${orderId}.pdf"`);
+      file.pipe(res);
+    })
+    .catch(err => next(err));
 };
