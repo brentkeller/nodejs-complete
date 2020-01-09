20021783c69a40065f3dc9130c71a8da8ae6982e
Brent Keller <brentkeller@gmail.com>
2020-01-09 15:11:35 -0500

    Add basic paging for products
    I deviated from the course material to try out adding a dynamic list page buttons. In a real use case this would need to be limited to 10 buttons or so.

--- controllers/shop.js
+++ controllers/shop.js
@@ -6,13 +6,32 @@ const PDFDocument = require('pdfkit');
 const Product = require('../models/product');
 const Order = require('../models/order');
 
+const ITEMS_PER_PAGE = 2;
+
 exports.getProducts = (req, res, next) => {
+  const page = +req.query.page || 1;
+  let totalProducts;
   Product.find()
+    .countDocuments()
+    .then(count => {
+      totalProducts = count;
+      return Product.find()
+        .skip((page - 1) * ITEMS_PER_PAGE)
+        .limit(ITEMS_PER_PAGE);
+    })
     .then(products => {
       res.render('shop/product-list', {
         prods: products,
         pageTitle: 'All Products',
         path: '/products',
+        currentPage: page,
+        lastPage: Math.ceil(totalProducts / ITEMS_PER_PAGE),
+        // I'm not using these in this app but they could be useful for other pagination scenarios
+        totalProducts,
+        hasNextPage: ITEMS_PER_PAGE * page < totalProducts,
+        hasPreviousPage: page > 1,
+        nextPage: page + 1,
+        previousPage: page - 1,
       });
     })
     .catch(err => {
@@ -40,12 +59,29 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
+  const page = +req.query.page || 1;
+  let totalProducts;
   Product.find()
+    .countDocuments()
+    .then(count => {
+      totalProducts = count;
+      return Product.find()
+        .skip((page - 1) * ITEMS_PER_PAGE)
+        .limit(ITEMS_PER_PAGE);
+    })
     .then(products => {
       res.render('shop/index', {
         prods: products,
         pageTitle: 'Shop',
         path: '/',
+        currentPage: page,
+        lastPage: Math.ceil(totalProducts / ITEMS_PER_PAGE),
+        // I'm not using these in this app but they could be useful for other pagination scenarios
+        totalProducts,
+        hasNextPage: ITEMS_PER_PAGE * page < totalProducts,
+        hasPreviousPage: page > 1,
+        nextPage: page + 1,
+        previousPage: page - 1,
       });
     })
     .catch(err => {
