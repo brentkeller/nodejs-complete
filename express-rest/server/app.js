0b688ec992f1c1d15cf395d9f1c391cd67e29224
Brent Keller <brentkeller@gmail.com>
2020-01-13 10:29:54 -0500

    Allow OPTIONS requests for /graphql
    This gives browsers a success response on the preflight OPTIONS request

--- express-rest/server/app.js
+++ express-rest/server/app.js
@@ -45,6 +45,8 @@ app.use((req, res, next) => {
     'GET, POST, PUT, PATCH, DELETE',
   );
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  // Allow OPTIONS requests since graphql handler will reject anything but GET/POST
+  if (req.method === 'OPTIONS') return res.sendStatus(200);
   next();
 });
 
